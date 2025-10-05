4e92d68aa89daa80da778f1c139a728b39992777
RamiusCode <ramius111@gmail>
2025-10-05 16:06:10 -0400

    Agregar slash final a base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ramiuscode.github.io',
-  base: '/xv-verde',
+  base: '/xv-verde/',
   vite: {
     plugins: [tailwindcss()]
   }
